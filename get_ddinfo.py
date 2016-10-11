c4c698474d758b1f0bcf2d88f248788e596afeb1
杨闯 <yangchuang@nationsky.com>
2016-10-11 21:07:36 +0800

    init

---
+++ get_ddinfo.py
@@ -0,0 +1,29 @@
+# coding:utf-8
+
+import requests
+from random import choice
+class get_ddinfo():
+
+    def __init__(self):
+        self.url = 'http://www.aokuu.net.cn/index.php/IH78fhgER04/Back/getdidan2'
+
+    def get_one_from_ddlist(self):
+        print "in get_dd_list step "
+
+        rt = requests.get(self.url)
+        rt_json = rt.json()
+        print rt.status_code
+        print 'total dd counts =',len(rt_json)
+        for k in rt_json:
+            print k
+
+        self.select_one_dd = choice(rt_json)
+        print self.select_one_dd
+        return self.select_one_dd
+
+
+
+
+if __name__ == "__main__":
+    get_ddinfo().get_dd_list()
+
